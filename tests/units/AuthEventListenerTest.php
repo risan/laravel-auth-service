6e0df0a85c2df1dd4f58f338e7460f4244a19ae9
risan <risanbagja@gmail.com>
2016-01-03 21:39:38 +0100

    Add test suites for AuthEventListener.

---
+++ tests/units/AuthEventListenerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+use Mockery as m;
+use AuthService\AuthEventListener;
+use Illuminate\Routing\Redirector;
+use Illuminate\Http\RedirectResponse;
+use AuthService\Contracts\AuthServiceConfig as AuthServiceConfigContract;
+
+class AuthEventListenerTest extends PHPUnit_Framework_TestCase {
+    protected $redirector;
+    protected $config;
+    protected $redirectResponse;
+    protected $eventListener;
+
+    function setUp()
+    {
+        $this->redirector = m::mock(Redirector::class);
+        $this->config = m::mock(AuthServiceConfigContract::class);
+        $this->redirectResponse = m::mock(RedirectResponse::class);
+        $this->eventListener = new AuthEventListener($this->redirector, $this->config);
+    }
+
+    function tearDown()
+    {
+        m::close();
+    }
+
+    /** @test */
+    function auth_event_listener_has_redirector()
+    {
+        $this->assertInstanceOf(Redirector::class, $this->eventListener->redirector());
+    }
+
+    /** @test */
+    function auth_event_listener_has_config()
+    {
+        $this->assertInstanceOf(AuthServiceConfigContract::class, $this->eventListener->config());
+    }
+
+    /** @test */
+    function auth_event_listener_can_handle_successfull_login()
+    {
+        $this->redirector->shouldReceive('intended')->times(1)->andReturn($this->redirectResponse);
+        $this->config->shouldReceive('afterLoginSuccessPath')->times(1);
+
+        $this->assertInstanceOf(RedirectResponse::class, $this->eventListener->userHasLoggedIn());
+    }
+
+    /** @test */
+    function auth_event_listener_can_handle_failed_login()
+    {
+        $this->redirector->shouldReceive('back')->times(1)->andReturn($this->redirectResponse);
+        $this->redirectResponse->shouldReceive('exceptInput')->times(1)->andReturn($this->redirectResponse);
+        $this->redirectResponse->shouldReceive('withErrors')->times(1)->andReturn($this->redirectResponse);
+        $this->config->shouldReceive('loginFailedMessage')->times(1);
+
+        $this->assertInstanceOf(RedirectResponse::class, $this->eventListener->userHasFailedToLogIn());
+    }
+
+    /** @test */
+    function auth_event_listener_can_handle_successfull_logout()
+    {
+        $this->redirector->shouldReceive('to')->times(1)->andReturn($this->redirectResponse);
+        $this->config->shouldReceive('afterLogoutSuccessPath')->times(1);
+
+        $this->assertInstanceOf(RedirectResponse::class, $this->eventListener->userHasLoggedOut());
+    }
+}
